d798e669a38440924ed1092298fa78b21e271ff6
yogeshmehare <40709255+yogeshmehare@users.noreply.github.com>
2019-05-18 12:43:01 +0530

    Add files via upload

---
+++ oe/database.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("sql113.epizy.com","epiz_23705026","B0EwiaclukPyLg","epiz_23705026_oe") or die('Database not connected');
+mysqli_connect("sql113.epizy.com","epiz_23705026","B0EwiaclukPyLg","epiz_23705026_oe") or die('Database not connected');
+
+?>
